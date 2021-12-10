4e970463b4f15d0c690fe305eeddb7a04832b303
Matt Graham <matt.sg@hotmail.com>
2021-12-10 23:51:25 +0000

    Initial

---
+++ src/main/java/net/fabricmc/pogtools/PogAxe.java
@@ -0,0 +1,22 @@
+package net.fabricmc.pogmatica;
+
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Formatting;
+import net.minecraft.world.World;
+
+import java.util.List;
+
+public class PogAxe extends Item {
+    public PogAxe(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack itemStack, World world, List<Text> tooltip, TooltipContext tooltipContext) {
+        tooltip.add(new TranslatableText("item.pogmatica.pogaxe.tooltip").formatted(Formatting.LIGHT_PURPLE));
+    }
+}

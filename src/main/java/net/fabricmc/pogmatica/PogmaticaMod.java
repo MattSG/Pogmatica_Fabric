5a9e76bfcb8bc1cbec829e83a64c6087e1eb76c8
Matt Graham <matt.sg@hotmail.com>
2021-12-12 01:03:54 +0000

    Added pogmail armor with temporary textures/models

--- src/main/java/net/fabricmc/pogmatica/PogmaticaMod.java
+++ src/main/java/net/fabricmc/pogmatica/PogmaticaMod.java
@@ -3,13 +3,14 @@ package net.fabricmc.pogmatica;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.pogmatica.armor.materials.PogMailArmorMaterial;
 import net.fabricmc.pogmatica.tools.Pagger;
 import net.fabricmc.pogmatica.tools.Patchet;
 import net.fabricmc.pogmatica.tools.Povel;
 import net.fabricmc.pogmatica.tools.materials.PogToolMaterial;
 import net.fabricmc.pogmatica.tools.PogAxe;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.*;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -21,10 +22,16 @@ public class PogmaticaMod implements ModInitializer {
             () -> new ItemStack(PogmaticaMod.PATCHET));
 
     // Tools
-    public static final Patchet PATCHET = new Patchet(PogToolMaterial.INSTANCE, 3.0F, -3.0F, new FabricItemSettings().group(ItemGroup.TOOLS).group(POGMATICA_TOOLS));
-    public static final PogAxe POGAXE = new PogAxe(PogToolMaterial.INSTANCE, 3, -3, new FabricItemSettings().group(ItemGroup.TOOLS).group(POGMATICA_TOOLS));
-    public static final Povel POVEL = new Povel(PogToolMaterial.INSTANCE, 3, -3, new FabricItemSettings().group(ItemGroup.TOOLS).group(POGMATICA_TOOLS));
-    public static final Pagger PAGGER = new Pagger(PogToolMaterial.INSTANCE, 3, -3, new FabricItemSettings().group(ItemGroup.TOOLS).group(POGMATICA_TOOLS));
+    public static final Item PATCHET = new Patchet(PogToolMaterial.INSTANCE, 3.0F, -3.0F, new FabricItemSettings().group(ItemGroup.TOOLS).group(POGMATICA_TOOLS));
+    public static final Item POGAXE = new PogAxe(PogToolMaterial.INSTANCE, 3, -3, new FabricItemSettings().group(ItemGroup.TOOLS).group(POGMATICA_TOOLS));
+    public static final Item POVEL = new Povel(PogToolMaterial.INSTANCE, 3, -3, new FabricItemSettings().group(ItemGroup.TOOLS).group(POGMATICA_TOOLS));
+    public static final Item PAGGER = new Pagger(PogToolMaterial.INSTANCE, 3, -3, new FabricItemSettings().group(ItemGroup.TOOLS).group(POGMATICA_TOOLS));
+
+    // Armor
+    public static final Item POGMAIL_HELMET = new ArmorItem(PogMailArmorMaterial.INSTANCE, EquipmentSlot.HEAD, new Item.Settings().group(POGMATICA_TOOLS));
+    public static final Item POGMAIL_CHESTPLATE = new ArmorItem(PogMailArmorMaterial.INSTANCE, EquipmentSlot.CHEST, new Item.Settings().group(POGMATICA_TOOLS));
+    public static final Item POGMAIL_LEGGINGS = new ArmorItem(PogMailArmorMaterial.INSTANCE, EquipmentSlot.LEGS, new Item.Settings().group(POGMATICA_TOOLS));
+    public static final Item POGMAIL_BOOTS = new ArmorItem(PogMailArmorMaterial.INSTANCE, EquipmentSlot.FEET, new Item.Settings().group(POGMATICA_TOOLS));
 
     @Override
     public void onInitialize() {
@@ -33,5 +40,11 @@ public class PogmaticaMod implements ModInitializer {
         Registry.register(Registry.ITEM, new Identifier("pogmatica", "pogaxe"), POGAXE);
         Registry.register(Registry.ITEM, new Identifier("pogmatica", "povel"), POVEL);
         Registry.register(Registry.ITEM, new Identifier("pogmatica", "pagger"), PAGGER);
+
+        // Register armor
+        Registry.register(Registry.ITEM, new Identifier("pogmatica", "pogmail_helmet"), POGMAIL_HELMET);
+        Registry.register(Registry.ITEM, new Identifier("pogmatica", "pogmail_chestplate"), POGMAIL_CHESTPLATE);
+        Registry.register(Registry.ITEM, new Identifier("pogmatica", "pogmail_leggings"), POGMAIL_LEGGINGS);
+        Registry.register(Registry.ITEM, new Identifier("pogmatica", "pogmail_boots"), POGMAIL_BOOTS);
     }
 }
---
+++ src/main/java/net/fabricmc/pogmatica/armor/materials/PogMailArmorMaterial.java
@@ -0,0 +1,55 @@
+package net.fabricmc.pogmatica.armor.materials;
+
+import net.fabricmc.pogmatica.tools.materials.PogToolMaterial;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+
+public class PogMailArmorMaterial implements ArmorMaterial {
+    public static final ArmorMaterial INSTANCE = new PogMailArmorMaterial();
+
+    private static final int[] BASE_DURABILITY = new int[] {10, 10, 10, 10};
+    private static final int[] PROTECTION_VALUES = new int[] {10, 10, 10, 10};
+
+    @Override
+    public int getDurability(EquipmentSlot slot) {
+        return BASE_DURABILITY[slot.getEntitySlotId()];
+    }
+
+    @Override
+    public int getProtectionAmount(EquipmentSlot slot) {
+        return PROTECTION_VALUES[slot.getEntitySlotId()];
+    }
+
+    @Override
+    public int getEnchantability() {
+        return 0;
+    }
+
+    @Override
+    public SoundEvent getEquipSound() {
+        return SoundEvents.ITEM_ARMOR_EQUIP_CHAIN;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return "pogmail";
+    }
+
+    @Override
+    public float getToughness() {
+        return 10;
+    }
+
+    @Override
+    public float getKnockbackResistance() {
+        return 0;
+    }
+}

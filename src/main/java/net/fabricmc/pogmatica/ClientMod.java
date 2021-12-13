f931a782f6e89752b44b49c767c585d766ac94f9
Matt Graham <matt.sg@hotmail.com>
2021-12-13 21:37:05 +0000

    Fixed file structure to allow correct display of crop textures
    - Sorted language file

--- src/main/java/net/fabricmc/pogmatica/ClientMod.java
+++ src/main/java/net/fabricmc/pogmatica/ClientMod.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.minecraft.client.render.RenderLayer;
 
-public class ModClient implements ClientModInitializer {
+public class ClientMod implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getCutout(), PogmaticaMod.ASPOGARUS);
